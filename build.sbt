79c738e0af087cbdae6315d44233d0562d720d99
Jarek Sacha <jpsacha@gmail.com>
2021-08-10 22:37:55 -0400

    Update SBT and dependencies

--- build.sbt
+++ build.sbt
@@ -1,21 +1,22 @@
 import Dependencies._
 
 // Use FIJI Scala version
-ThisBuild / scalaVersion := "2.12.13"
+ThisBuild / scalaVersion := "2.12.14"
+//ThisBuild / scalaVersion := "2.13.6"
 
-ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / version := "0.1.0.1-SNAPSHOT"
 ThisBuild / organization := "net.sf.ij-plugins"
 ThisBuild / organizationName := "IJ-Plugins"
 
 // Determine OS version of JavaFX binaries
-lazy val osName = System.getProperty("os.name") match {
+lazy val osName        = System.getProperty("os.name") match {
   case n if n.startsWith("Linux") => "linux"
   case n if n.startsWith("Mac") => "mac"
   case n if n.startsWith("Windows") => "win"
   case _ => throw new Exception("Unknown platform!")
 }
 lazy val javaFXModules = Seq("base", "controls", "fxml", "graphics", "media", "swing", "web")
-lazy val javaFXVersion = "15.0.1"
+lazy val javaFXVersion = "16"
 
 lazy val root = (project in file("."))
   .settings(
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 // @formatter:off
 object Dependencies {
-  lazy val ijpColor             = "net.sf.ij-plugins" %% "ijp-color"              % "0.10.1"
-  lazy val ijpColorUI           = "net.sf.ij-plugins" %% "ijp-color-ui"           % "0.10.1"
-  lazy val ijpDebayer2sx        = "net.sf.ij-plugins" %% "ijp-debayer2sx-core"    % "1.2.0"
-  lazy val ijpDebayer2sxPlugins = "net.sf.ij-plugins" %% "ijp-debayer2sx-plugins" % "1.2.0"
+  lazy val ijpColor             = "net.sf.ij-plugins" %% "ijp-color"              % "0.10.2"
+  lazy val ijpColorUI           = "net.sf.ij-plugins" %% "ijp-color-ui"           % "0.10.2"
+  lazy val ijpDebayer2sx        = "net.sf.ij-plugins" %% "ijp-debayer2sx-core"    % "1.3.0"
+  lazy val ijpDebayer2sxPlugins = "net.sf.ij-plugins" %% "ijp-debayer2sx-plugins" % "1.3.0"
   lazy val ijpImageIO           = "net.sf.ij-plugins"  % "ijp_imageio"            % "2.2.0"
-  lazy val ijpToolkit           = "net.sf.ij-plugins" %% "ijp-toolkit"            % "2.3.0"
+  lazy val ijpToolkit           = "net.sf.ij-plugins" %% "ijp-toolkit"            % "2.3.1"
   lazy val ijpJavaCVCore        = "net.sf.ij-plugins" %% "ijp-javacv-core"        % "0.3.1"
-  lazy val imagej               = "net.imagej"         % "ij"                     % "1.53g"
-  lazy val scalaTest            = "org.scalatest"     %% "scalatest"              % "3.2.4"
+  lazy val imagej               = "net.imagej"         % "ij"                     % "1.53j"
+  lazy val scalaTest            = "org.scalatest"     %% "scalatest"              % "3.2.9"
 }
